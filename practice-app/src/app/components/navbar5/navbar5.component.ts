f41190a9aedd3e4e111d497b72d5c205ec5b2cdc
unknown <mantuciprian2011@gmail.com>
2018-08-22 14:07:00 +0300

    nav5 finished

--- practice-app/src/app/components/navbar5/navbar5.component.ts
+++ practice-app/src/app/components/navbar5/navbar5.component.ts
@@ -9,16 +9,29 @@ export class Navbar5Component implements OnInit {
 
   showTabs: boolean;
   dontAnim: boolean;
+  showBlog: boolean;
+  dontAnimBlog:boolean;
   constructor() { }
 
   ngOnInit() {
     this.showTabs = false;
     this.dontAnim = true;
+    this.showBlog = false;
+    this.dontAnimBlog = true;
   }
+
 
   toggleTabs() {
     this.showTabs = !this.showTabs;
     this.dontAnim = false;
+    this.showBlog = false;
+    this.dontAnimBlog = true;
+    
+  }
+
+  toggleBlog() {
+    this.showBlog = !this.showBlog ;
+    this.dontAnimBlog = false;
   }
 
 
